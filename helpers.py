363baf3cea36934d7a8034f8b3aadb7da7631c63
Volodymyr Lyubinets <vlyubin@volodymyrs-macbook-pro.home>
2017-10-03 23:47:45 +0200

    Added some basics, and a sample solution using an SVM, including the SVM implemetation.
    Updated gitignore to ignore test.csv - it's too large for github
    
    Added the first submission

--- helpers.py
+++ helpers.py
@@ -2,11 +2,42 @@ import numpy as np
 import pandas as pd
 
 '''
-This function reads a CSV file from a given location into a pandas datafrmame.
-This can be converted to numpy
+This function reads training data from CSV file in a given location into a pandas datafrmame.
 '''
-def read_experiment_data(fname):
+def read_train_data(fname):
   data = pd.read_csv(fname, sep=',')
-  X = data.drop('Prediction', axis=1)
+  X = data.drop('Prediction', axis=1).drop('Id', axis=1)
   y = data['Prediction']
-  return X, y
+  replace_classes = lambda x: 0 if x == 'b' else 1
+  y = y.map(replace_classes)
+  return np.array(X), np.array(y)
+
+'''
+This function reads test data from CSV file in a given location into a pandas datafrmame.
+'''
+def read_test_data(fname):
+  data = pd.read_csv(fname, sep=',')
+  X = data.drop('Prediction', axis=1).drop('Id', axis=1)
+  ids = data['Id']
+  return np.array(X), np.array(ids)
+
+'''
+Makes each column to be Gaussian(0, 1)
+'''
+def standardize(x):
+  cd = x - np.mean(x, axis=0)
+  std_data = cd / np.std(cd, axis=0)  
+  return std_data
+
+'''
+Computes means for each column, while skipping invalid values. Results are
+groupped by the assigned class (b or s).
+'''
+def compute_means_for_columns(data):
+  pass
+
+'''
+Replaces -999 in the given data with means computed by compute_means_for_columns()
+'''
+def replace_missing_values_with_means(data, means):
+  pass
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import numpy as numpy
+import pandas as pd
 from helpers import *
+from classifiers import *
 
 '''
 Runs the clasification pipeline. In the end this should produce a file
 called prediction.csv with test set classification.
 '''
 def run():
-  X_train, y_train = read_experiment_data('datasets/train.csv')
-  # TODO: do something smart :)
+  X_train, y_train = read_train_data('datasets/train.csv')
+  svm = SVM()
+  # Train the SVM
+  svm.fit(X_train, y_train, verbose=True)
+
+  X_test, X_test_ids = read_test_data('datasets/test.csv')
+  test_predictions = svm.predict(X_test)
+  # HACK: Right now predictions are 0,1 , and we need -1,1
+  test_predictions = 2 * test_predictions
+  test_predictions = test_predictions - 1
+
+  test_predictions = pd.DataFrame(np.array([X_test_ids, test_predictions]).T, columns=['Id', 'Prediction'])
+  test_predictions.to_csv('prediction.csv', sep=',', columns=['Id', 'Prediction'], index=False)
 
 if __name__ == '__main__':
   run()

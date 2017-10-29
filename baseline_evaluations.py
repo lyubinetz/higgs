35a675c7bfbedb0d77e2c2a1cca7004f2f4df759
Doru Musuroi <doru.musuroi@gmail.com>
2017-10-29 19:21:03 +0100

    Added benchmarks and bias-variance decomposition code

---
+++ baseline_evaluations.py
@@ -0,0 +1,122 @@
+from base_methods_wrappers import *
+from implementations import *
+from featurization import *
+
+def create_method_params(method_name, shape=30):
+  '''
+  Creates a dictionary with parameters for the method defined in implementation.py specified by method_name
+  :param method_name: values set: linear_regression, ridge_regression, reg_log_reg (i.e. regularized logistic
+  regression)
+  :param shape: number of features in the dataset
+  :return: dictionary with parameter_name:value
+  '''
+  if method_name == 'linear_regression':
+    method_params = {}
+  elif method_name == 'ridge_regression':
+    method_params = {
+      'lambda_':0.01
+    }
+  elif method_name == 'reg_log_reg':
+    method_params = {
+      'lambda_':0.01,
+      'initial_w':np.zeros((shape, 1)),
+      'max_iters':1000,
+      'gamma':0.000001
+    }
+  else:
+    raise Exception('Wrong method name in baseline evaluation')
+
+  return method_params
+
+
+def get_method(method_name):
+  '''
+  Simply returns the method defined in implmenetations.py with the correspoding name
+  :param method_name:  values set: linear_regression, ridge_regression, reg_log_reg (regularized logistic regression)
+  :return: method
+  '''
+  if method_name == 'linear_regression':
+    return least_squares
+  if method_name == 'ridge_regression':
+    return ridge_regression
+  if method_name == 'reg_log_reg':
+    return reg_logistic_regression
+  raise Exception("Wrong method name in get_method baseline evaluation")
+
+
+def evaluate(X, y, method_name, log_file_name ="logs.tx", verbose=False):
+  '''
+  Evaluates the the result of the learning method specified by method_name over a 3-fold cross-validation.
+  The evaluation result is appended in the log_file_name file
+  :param X: train data
+  :param y: train labels
+  :param method_name: method to evaluate
+  :param log_file_name: file where to append the evaluation result
+  :param verbose:
+  :return: Nothing
+  '''
+  if verbose:
+    print('Started evaluation run for {}'.format(method_name))
+
+  learn_method_params = create_method_params(method_name, X.shape[1])
+  if verbose:
+    print('Learn parameters {}'.format(learn_method_params))
+
+  wrapper_constructor_params = {
+    'method':get_method(method_name),
+    'method_params':learn_method_params
+  }
+  loss_tr, loss_te = cross_validate(
+    BaseMethodWrapper,
+    y,
+    X,
+    3,
+    lambda y_pred, y_test: (y_pred == y_test).sum()*100/len(y_pred),
+    wrapper_constructor_params,
+    {},
+    777,
+    True
+  )
+
+  with open(log_file_name, 'a') as f:
+    f.write("{} obtained results:\ntrain: {}\ntest: {}\n with params: {}\n input shape: {}".format(method_name,
+                                                                                                   loss_tr, loss_te,
+                                                                                                   learn_method_params,
+                                                                                                   X.shape[1]))
+    f.write('\n\n')
+    f.flush()
+
+def compute_featurized_data(X_train, X_combined, featurization_method = None):
+  if featurization_method is None:
+    good_featurized_means, good_featurized_vars = compute_means_and_vars_for_columns(X_combined)
+    X_train = standardize(X_train, good_featurized_means, good_featurized_vars)
+  elif featurization_method == 'x^2':
+    good_featurized_means, good_featurized_vars = compute_means_and_vars_for_columns(featurize_x2(X_combined))
+    X_train = standardize(featurize_x2(X_train), good_featurized_means, good_featurized_vars)
+  elif featurization_method == 'final':
+    good_featurized_means, good_featurized_vars = compute_means_and_vars_for_columns(featurize_before_standardize(X_combined))
+    X_train = featurize_and_standardize(X_train, mean=good_featurized_means, var=good_featurized_vars)
+
+  return X_train
+
+
+if __name__ == '__main__':
+  print('Started the run!')
+  X_train, y_train = read_train_data('datasets/train.csv', load_pickle=True)
+  X_test, X_test_ids = read_test_data('datasets/test.csv', load_pickle=True)
+
+  print('Finished loading data!')
+
+  X_combined = np.vstack((X_train, X_test))
+  mean_map, var_map = compute_means_and_vars_for_columns(X_combined)
+
+  replace_missing_values(X_combined, mean_map)
+  replace_missing_values(X_train, mean_map)
+
+  featurization_methods = [None, 'x^2','final']
+  learning_methods = ['linear_regression', 'ridge_regression','reg_log_reg']
+
+  for feat_met in featurization_methods:
+    X = compute_featurized_data(X_train, X_combined, feat_met)
+    for learn_met in learning_methods:
+      evaluate(X, y_train, learn_met, 'result.txt', verbose=True)
---
+++ bias_variance_decomposition.py
@@ -0,0 +1,155 @@
+from base_methods_wrappers import *
+from implementations import *
+from featurization import *
+
+
+def bias_variance_decomposition_visualization(complexity_regulator, rmse_tr, rmse_te, x_label, y_label):
+  """visualize the bias variance decomposition."""
+  rmse_tr_mean = np.expand_dims(np.mean(rmse_tr, axis=0), axis=0)
+  rmse_te_mean = np.expand_dims(np.mean(rmse_te, axis=0), axis=0)
+  fig = plt.figure()
+  ax = fig.add_subplot(111)
+  ax.plot(
+    complexity_regulator,
+    rmse_tr.T,
+    'b',
+    linestyle="-",
+    color=([0.7, 0.7, 1]),
+    label='train',
+    linewidth=0.3)
+  ax.plot(
+    complexity_regulator,
+    rmse_te.T,
+    'r',
+    linestyle="-",
+    color=[1, 0.7, 0.7],
+    label='test',
+    linewidth=0.3)
+  ax.plot(
+    complexity_regulator,
+    rmse_tr_mean.T,
+    'b',
+    linestyle="-",
+    label='train',
+    linewidth=3)
+  ax.plot(
+    complexity_regulator,
+    rmse_te_mean.T,
+    'r',
+    linestyle="-",
+    label='test',
+    linewidth=3)
+  #fig.ylim(np.min(complexity_regulator), np.max(complexity_regulator))
+  plt.xlabel(x_label)
+  plt.ylabel(y_label)
+  plt.title("Bias-Variance Decomposition")
+  fig.savefig("bias_variance_{}".format(x_label))
+
+
+def bias_var_decomp_logistic_regression_regularizer(X_train, y_train):
+  lambdas = [ 0.001, 4][::-1]
+  seeds = range(5)
+
+  # define list to store the variable
+  rmse_tr = np.empty((len(seeds), len(lambdas)))
+  rmse_te = np.empty((len(seeds), len(lambdas)))
+
+  for index_seed, seed in enumerate(seeds):
+    fold_indices = build_k_indices(y_train, 200, seed)[0]
+    for index_lambda, lambda_ in enumerate(lambdas):
+      learn_method_params = {
+        'lambda_':lambda_,
+        'initial_w':np.zeros((X_train.shape[1]+1,1)),
+        'max_iters':200,
+        'gamma':0.0000001,
+        'sgd':False
+      }
+
+      wrapper_constructor_params = {
+        'method':reg_logistic_regression,
+        'method_params':learn_method_params
+      }
+
+      learner = BaseMethodWrapper(**wrapper_constructor_params)
+
+      loss_tr, loss_te = train_and_evaluate(learner,
+                         y_train,
+                         stack_ones(X_train),
+                         fold_indices,
+                         lambda y_test, y_pred : (y_pred == y_test).sum()*100/len(y_pred),
+                         {}
+                         )
+      rmse_tr[index_seed, index_lambda] = 1-loss_tr/100
+      rmse_te[index_seed, index_lambda] = 1-loss_te/100
+
+  bias_variance_decomposition_visualization(lambdas, rmse_tr, rmse_te, 'lambda', 'accuracy')
+
+
+def bias_var_logistic_reg_pbf_degree(X, y_train, X_c):
+  print("pbf degree")
+  degrees = [2,3,4,5,7]
+  seeds = range(5)
+
+  # define list to store the variable
+  rmse_tr = np.empty((len(seeds), len(degrees)))
+  rmse_te = np.empty((len(seeds), len(degrees)))
+
+  for index_seed, seed in enumerate(seeds):
+    fold_indices = build_k_indices(y_train, 200, seed)[0]
+    for index_degree, degree in enumerate(degrees):
+      X_comb_fit = build_poly(X_c, degree)
+      means, vars = compute_means_and_vars_for_columns(X_comb_fit)
+      X_train = standardize(build_poly(X, degree), means, vars)
+      learn_method_params = {
+        'lambda_':0.01,
+        'initial_w':np.zeros((X_train.shape[1]+1,1)),
+        'max_iters':200,
+        'gamma':0.0000001,
+        'sgd':False
+      }
+
+      wrapper_constructor_params = {
+        'method':reg_logistic_regression,
+        'method_params':learn_method_params
+      }
+
+      learner = BaseMethodWrapper(**wrapper_constructor_params)
+
+      loss_tr, loss_te = train_and_evaluate(learner,
+                                            y_train,
+                                            stack_ones(X_train),
+                                            fold_indices,
+                                            lambda y_test, y_pred : (y_pred == y_test).sum()*100/len(y_pred),
+                                            {}
+                                            )
+      rmse_tr[index_seed, index_degree] = 1-loss_tr/100
+      rmse_te[index_seed, index_degree] = 1-loss_te/100
+
+  bias_variance_decomposition_visualization(degrees, rmse_tr, rmse_te, 'degree', 'accuracy')
+
+
+if __name__ == "__main__":
+  print('Started the run!')
+  X_train, y_train = read_train_data('datasets/train.csv', load_pickle=True)
+  pbf = False 
+  X_test, X_test_ids = read_test_data('datasets/test.csv', load_pickle=True)
+
+  print('Finished loading data!')
+
+  X_combined = np.vstack((X_train, X_test))
+  mean_map, var_map = compute_means_and_vars_for_columns(X_combined)
+
+  # Compute featurzied means
+  replace_missing_values(X_combined, mean_map)
+  replace_missing_values(X_train, mean_map)
+
+  if not pbf:
+    good_featurized_means, good_featurized_vars = compute_means_and_vars_for_columns(featurize_before_standardize(X_combined))
+
+    X_train = featurize_and_standardize(X_train, mean=good_featurized_means, var=good_featurized_vars)
+
+    print('New number of features is ' + str(X_train.shape[1]))
+    print('Finished data ops!')
+    bias_var_decomp_logistic_regression_regularizer(X_train, y_train)
+  else:
+    bias_var_logistic_reg_pbf_degree(X_train, y_train, X_combined)
